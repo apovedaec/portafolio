6221e2a1ad1d26c07167b1b8e7d3b90290a67178
Angel Poveda <angelpk79@gmail.com>
2019-12-01 10:52:13 -0500

    Fin de seccion 4

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargado = false;
+
+  constructor(private http: HttpClient ) {
+    console.log('Servicio de informacion pagina listo');
+    this.http.get('assets/data/data-pagina.json').subscribe((resp: InfoPagina) => {
+        this.cargado = true;
+        this.info = resp;
+        console.log(this.info);
+    });
+  }
+}
